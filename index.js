5d973bec5c16698d9166ccb66cb74c678bde6149
thenandinii <parasharnandini15@gmail.com>
2025-06-07 22:23:43 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res){
+    fs.readdir(`./files`,function(err,files){
+        console.log(files); 
+        res.render("index", {files:files});
+}) })
+app.get("/file/:filename", function(req, res){
+   fs.readFile(`./files/${req.params.filename}`, "utf-8", function (err,filedata){
+    res.render('show', {filename: req.params.filename, filedata: filedata});
+   })
+}) 
+app.post("/file/:filename", function(req, res) {
+    fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}`, function(err) {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Error renaming file");
+        } else {
+            res.redirect("/");
+        }
+    });
+});
+
+
+app.get("/edit/:filename", function(req, res){
+  res.render('edit', {filename: req.params.filename}); ;
+}) 
+
+
+app.post("/create", function(req, res){
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err){})
+    res.redirect("/")
+})
+   
+
+
+app.listen(3000, function(){
+    console.log("its running")
+})
